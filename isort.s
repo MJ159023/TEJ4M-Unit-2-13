b43a8e9727ac47ff109e0d86a516c660ae3193cd
Michael-John <michael.bruneau@stu.ocsb.ca>
2025-12-28 21:03:41 -0500

    save

--- isort.s
+++ isort.s
@@ -55,6 +55,21 @@ iloopend:
     mov r10, #0 @ resets r10 to intial value
     bx lr @ returns to main
 
+@ gets strlen of string inputed
+my_strlen:
+    ldrb r5, [r0] @ r1 <- one byte of *r0 and then move to next byte
+    add r0, r0, #1 @ add one to pointer
+    cmp r5, #0
+    beq end_strlen @ if byte is empty the string is done
+    add r1, r1, #1 @ r1 <- r1 + 1
+    b my_strlen @ loops until string is empty
+end_strlen:
+    ldr r2, =length @ r2 <- &length
+    str r1, [r2] @ *r2 <- r1
+    mov r5, #0 @ resets r5
+    bx lr
+
+
 .global main
 main:
     @@@@@@@@@@@@@@@@@ INITIALIZE
@@ -68,18 +83,27 @@ input:
     ldr r0, =prompt
     bl puts
     ldr r0, =scanFMT @ r0 <- &scan format
-    ldr r1, =number @ r1 <- &number
+    ldr r1, =buffer @ r1 <- &buffer
     bl scanf @ call to scanf
-    ldr r1, =number
-    ldr r1, [r1]
-    cmp r1, #0 @ look for sentinel (negative)
-    blt sort @ goto isort function
-    add r0, r4, r6, LSL #2 @ r0 <- &array[4*count]
-    str r1, [r0] @ array[4*count] <- number
-    add r6, r6, #1 @ count = count + 1
-    ldr r5, =length @ r5 <- &length
-    str r6, [r5] @ *r5 <- r6 = count
-    b input
+
+    ldr r0, =buffer @ r0 <- &buffer
+    ldr r1, =length @ r1 <- &length
+    ldr r1, [r1] @ r1 <- *r1
+    bl my_strlen @ calls my_strlen
+
+    ldr r7, =length @ r7 <- &length
+    ldr r7, [r7] @ keep r7 as constant for length
+
+    @ loops input into array for sorting
+loop:
+    ldr r0, =buffer @ r0 <- &buffer
+    ldrb r0, [r0] @ r0 <- *r0 stores one byte of ascii
+    add r0, r0, #1 @ r0 <- r0 + 1
+    add r6, r6, #1 @ r6 <- r6 + 1
+
+    cmp r7, r6
+    ble sort @ if r7 <= r6 branch to sort
+    b loop @ loop program
 
 sort:
     ldr r0, =array @ r0 <- &array
@@ -109,13 +133,13 @@ exit:
 
 @@@@@@@@@@@@@@@@@
 .data
-number: .word 0 @ place to hold input number
+buffer: .space 100 @ creates space for 100 ascii bytes
 length: .word 0 @ place to hold length of array
 array: .space 100 @ room for 25 integers = 100 bytes
 return: .word 0 @ place for return address of main
 prompt: .asciz "Input a positive integer (negative to quit): "
 result: .asciz "Sorted, those integers are: \n"
-scanFMT: .asciz "%d"
+scanFMT: .asciz "%s"
 printFMT: .asciz " %d\n"
 @@@@@@@@@@@@@@@@@
 /* External */
