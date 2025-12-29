d50256067d3e01afa779ee9e9eb277ea3836e469
Michael-John <michael.bruneau@stu.ocsb.ca>
2025-12-29 17:07:21 -0500

    save

--- rand.s
+++ rand.s
@@ -16,7 +16,7 @@ str lr, [r1]
 
 mov r0, #0x7a
 mov r0, r0, LSL #8
-add r0, r0, #0x68 @ X0 = r0 = 0x7a68 = 31416
+add r0, r0, #0xb8 @ X0 = r0 = 0x7a68 = 31416
 
 /* ldr r4, #32445 @ a - initialized */
 
@@ -29,17 +29,18 @@ add r4, r4, #0xbd @ a = r4 = 0x7ebd = 32445
 mov r5, #0xFF
 mov r5, r5, LSL #8
 add r5, r5, #0xFF @ mask = m-1 = 0x0000FFFF
-ldr r6, #396 @ counter - initialized 4*100-4 for 100 ints
-ldr r7, #100 @ limit - initialized so values 0-99
+mov r6, #396 @ counter - initialized 4*100-4 for 100 ints
+mov r7, #100 @ limit - initialized so values 0-99
 Loop: @ while counter < 100
 cmp r6, #0 @ check counter
 blt Exit @ Stop when counter passes zero
-mul r0, r0, r4 @ X = aX (mul works like this)
+mul r2, r0, r4 @ X = aX (mul works like this)
+mov r0, r2 @ r0 <- r2
 add r0, #1 @ now X = aX+c
 and r0, r0, r5 @ now X = (aX+c) mod m
 mov r8, r0 @ save X in r8 temporarily
-lsr a0, a0, 8 @ divide by 256 (use upper 8 bits)
-cmp a0, r7 @ check size
+add r0, r0, lsr #8 @ divide by 256 (use upper 8 bits) check
+cmp r0, r7 @ check size
 bge Loop @ only want those < 100
 @Print
 ldr r0, =format
@@ -48,13 +49,13 @@ bl printf
 @Store
 mov r0, r8 @ put X back
 ldr r1, =list @ prepare to store
-str r0, r1, [r6 -#4] @ store and then decrement counter
+str r0, [r1, #-4] @ store and then decrement counter
 
 @End_of_Loop
 b Loop
 Exit:
 ldr lr, =return
-lrd lr, [lr] @ standard return to OS
+ldr lr, [lr] @ standard return to OS
 bx lr
 @
 .data
@@ -64,3 +65,4 @@ format: .asciz " %d "
 @
 /* External */
 .global printf
+
