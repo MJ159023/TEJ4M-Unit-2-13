ceea45f01eea6a0c1a82233784d4dafc5133bf0c
Michael-John <michael.bruneau@stu.ocsb.ca>
2025-12-29 19:20:42 -0500

    save

--- rand.s
+++ rand.s
@@ -10,60 +10,62 @@ using X0 = 31416 in r0 as seed (could be input)
 .text
 .global main
 main:
-ldr r1, =return @ save return address
-str lr, [r1]
-/* ldr r0, #31416 @ X0 - initialized (could be input) */
+    ldr r1, =return @ save return address
+    str lr, [r1]
+    @ X0 - initialized (input)
 
-ldr r0, =message @ r0 <- &message
-bl puts @ call puts
+    ldr r0, =message @ r0 <- &message
+    bl puts @ call puts
 
-ldr r0, =scanFMT @ r0 <- &scanFMT
-ldr r1, =number @ r1 <- &number
-bl scanf @ call scanf
-ldr r0, =return @ r0 <- &number
-ldr r0, [r0] @ r0 <- *r0
+    ldr r0, =scanFMT @ r0 <- &scanFMT
+    ldr r1, =number @ r1 <- &number
+    bl scanf @ call scanf
+    ldr r0, =number @ r0 <- &number
+    ldr r0, [r0] @ r0 <- *r0
 
-/* ldr r4, #32445 @ a - initialized */
+    /* ldr r4, #32445 @ a - initialized */
 
-mov r4, #0x7e
-mov r4, r4, LSL #8
-add r4, r4, #0xbd @ a = r4 = 0x7ebd = 32445
+    mov r4, #0x7e
+    mov r4, r4, LSL #8
+    add r4, r4, #0xbd @ a = r4 = 0x7ebd = 32445
 
-/* ldr r5, #0x0000FFFF @ mask to do modulo (m-1) - initialized */
+    /* ldr r5, #0x0000FFFF @ mask to do modulo (m-1) - initialized */
 
-mov r5, #0xFF
-mov r5, r5, LSL #8
-add r5, r5, #0xFF @ mask = m-1 = 0x0000FFFF
-mov r6, #396 @ counter - initialized 4*100-4 for 100 ints
-mov r7, #100 @ limit - initialized so values 0-99
-Loop: @ while counter < 100
-cmp r6, #0 @ check counter
-blt Exit @ Stop when counter passes zero
-mul r2, r0, r4 @ X = aX (mul works like this)
-mov r0, r2 @ r0 <- r2
-add r0, #1 @ now X = aX+c
-and r0, r0, r5 @ now X = (aX+c) mod m
-mov r8, r0 @ save X in r8 temporarily
-add r0, r0, lsr #8 @ divide by 256 (use upper 8 bits) check
-cmp r0, r7 @ check size
-bge Loop @ only want those < 100
-@Print
-ldr r0, =format
-mov r1, r8 @ prepare to print
-bl printf
-@Store
-mov r0, r8 @ put X back
-ldr r1, =list @ prepare to store
-str r0, [r1, #-4] @ store and then decrement counter
-add r6, r6, #-4 @ r6 <- r6 - 4
+    mov r5, #0xFF
+    mov r5, r5, LSL #8
+    add r5, r5, #0xFF @ mask = m-1 = 0x0000FFFF
+    mov r6, #396 @ counter - initialized 4*100-4 for 100 ints
+    mov r7, #100 @ limit - initialized so values 0-99
+    Loop: @ while counter < 100
+    cmp r6, #0 @ check counter
+    blt Exit @ Stop when counter passes zero
+    mul r2, r0, r4 @ X = aX (mul works like this)
+    mov r0, r2 @ r0 <- r2
+    add r0, #1 @ now X = aX+c
+    and r0, r0, r5 @ now X = (aX+c) mod m
+    mov r8, r0 @ save X in r8 temporarily
+    add r9, r0, lsr #8 @ divide by 256 (use upper 8 bits) check
+    cmp r9, r7 @ check size
+    bge Loop @ only want those < 100
 
-@End_of_Loop
-b Loop
+    @Print
+    ldr r0, =format
+    mov r1, r8 @ prepare to print
+    bl printf
+
+    @Store
+    mov r0, r8 @ put X back
+    ldr r1, =list @ prepare to store
+    str r0, [r1, r6] @ store and then decrement counter
+    add r6, r6, #-4 @ r6 <- r6 - 4
+
+    @End_of_Loop
+    b Loop
 Exit:
-ldr lr, =return
-ldr lr, [lr] @ standard return to OS
-bx lr
-@
+    ldr lr, =return
+    ldr lr, [lr] @ standard return to OS
+    bx lr
+
 .data
 list: .space 400 @ room for 100 integers
 number: .word 0 @ reprsents X0
