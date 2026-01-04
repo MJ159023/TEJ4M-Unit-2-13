d3e47b7b8702af5d63cd97b3f92fe0e00678fe24
MJ159023 <michael.bruneau@stu.ocsb.ca>
2026-01-04 17:36:28 +0000

    save

--- test01.s
+++ test01.s
@@ -9,4 +9,13 @@ heap:
     str r1, [sp, #4] @ r1 = n
     str lr, [sp, #8]
 
-    
+    cmp r1, #1
+    beq base_case @ branch if n == 1 to base_case
+
+for_loop:
+    cmp r0, r1
+    bge end: @ does for loop as long as i < n
+    sub r1, r1, #1 @ n = n -1
+    bl heap @ call heap
+
+    and 
